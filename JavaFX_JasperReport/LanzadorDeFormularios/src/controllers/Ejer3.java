721b98cd47ce14a8052b5562608670bfc2667506
HidalgoJoni <jonigarciah@gmail.com>
2024-01-30 11:56:03 +0100

    UPDATE Cambios en el controlador del ejercicio 3 y en el primer apartado del ejercicio 3

--- JavaFX_JasperReport/LanzadorDeFormularios/src/controllers/Ejer3.java
+++ JavaFX_JasperReport/LanzadorDeFormularios/src/controllers/Ejer3.java
@@ -1,18 +1,14 @@
 package controllers;
 
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
-import javafx.stage.Stage;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
@@ -21,168 +17,56 @@ import net.sf.jasperreports.view.JasperViewer;
 
 public class Ejer3 {
 
-
-
-	@FXML
-    private Button btnG;
+    @FXML
+    private Button btnG, btnP, btnS, btnT;
 
     @FXML
-    private Button btnP;
+    private Tab pestaniaG, pestaniaP, pestaniaS, pestaniaT;
 
-    @FXML
-    private Button btnS;
-
-    @FXML
-    private Button btnT;
-
-    @FXML
-    private Tab pestaniaG;
-
-    @FXML
-    private Tab pestaniaP;
-
-    @FXML
-    private Tab pestaniaS;
-
-    @FXML
-    private Tab pestaniaT;
-
-    
-    @FXML
-	void initialize() {
-    	
-	}
     @FXML
     void pestania1(ActionEvent event) {
-    	
-    	Connection connection = null;
-
-		try {
-			String url = "jdbc:mysql://localhost:3306/supermercado";
-			String user = "javauser";
-			String password = "Javajava0";
-
-			connection = DriverManager.getConnection(url, user, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-
-		try {
-			
-			JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getResource("/Ejer3_3.jasper"));
-			JasperPrint jprint = JasperFillManager.fillReport(report, null, connection);
-			JasperViewer viewer = new JasperViewer(jprint, false);
-			viewer.setVisible(true);
-		} catch (Exception e) {
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setHeaderText(null);
-			alert.setTitle("ERROR");
-			alert.setContentText("Ha ocurrido un error");
-			alert.showAndWait();
-			e.printStackTrace();
-		}
+        manejarReporte("/Ejer3_3.jasper");
     }
 
     @FXML
     void pestania2(ActionEvent event) {
-    	
-    	Connection connection = null;
-
-		try {
-			String url = "jdbc:mysql://localhost:3306/supermercado";
-			String user = "javauser";
-			String password = "Javajava0";
-
-			connection = DriverManager.getConnection(url, user, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			
-			JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getResource("/Ejer3_2.jasper"));
-			JasperPrint jprint = JasperFillManager.fillReport(report, null, connection);
-			JasperViewer viewer = new JasperViewer(jprint, false);
-			viewer.setVisible(true);
-		} catch (Exception e) {
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setHeaderText(null);
-			alert.setTitle("ERROR");
-			alert.setContentText("Ha ocurrido un error");
-			alert.showAndWait();
-			e.printStackTrace();
-		}
+        manejarReporte("/Ejer3_2.jasper");
     }
 
     @FXML
     void pestania3(ActionEvent event) {
-    	
-    	Connection connection = null;
-
-		try {
-			String url = "jdbc:mysql://localhost:3306/supermercado";
-			String user = "javauser";
-			String password = "Javajava0";
-
-			connection = DriverManager.getConnection(url, user, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-
-		try {
-			
-			JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getResource("/Ejer3_1.jasper"));
-			JasperPrint jprint = JasperFillManager.fillReport(report, null, connection);
-			JasperViewer viewer = new JasperViewer(jprint, false);
-			viewer.setVisible(true);
-		} catch (Exception e) {
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setHeaderText(null);
-			alert.setTitle("ERROR");
-			alert.setContentText("Ha ocurrido un error");
-			alert.showAndWait();
-			e.printStackTrace();
-		}
+        manejarReporte("/Ejer3_1.jasper");
     }
 
     @FXML
-    void pestania4(ActionEvent event) throws SQLException {
-    	
-    	Connection connection = null;
-
-		try {
-			String url = "jdbc:mysql://localhost:3306/supermercado";
-			String user = "javauser";
-			String password = "Javajava0";
-
-			connection = DriverManager.getConnection(url, user, password);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			
-			JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getResource("/Ejer3_4.jasper"));
-			JasperPrint jprint = JasperFillManager.fillReport(report, null, connection);
-			JasperViewer viewer = new JasperViewer(jprint, false);
-			viewer.setVisible(true);
-		} catch (Exception e) {
-			Alert alert = new Alert(Alert.AlertType.ERROR);
-			alert.setHeaderText(null);
-			alert.setTitle("ERROR");
-			alert.setContentText("Ha ocurrido un error");
-			alert.showAndWait();
-			e.printStackTrace();
-		}
-		 connection.close();
+    void pestania4(ActionEvent event) {
+        manejarReporte("/Ejer3_4.jasper");
     }
 
-   
-	
-	void salir(ActionEvent event) {
-		((Stage)((Node)event.getSource()).getScene().getWindow()).close();
-	}
-}
+    private void manejarReporte(String reportPath) {
+        try (Connection connection = obtenerConexion()) {
+            JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getResource(reportPath));
+            JasperPrint jprint = JasperFillManager.fillReport(report, null, connection);
+            JasperViewer viewer = new JasperViewer(jprint, false);
+            viewer.setVisible(true);
+        } catch (Exception e) {
+            mostrarError();
+            e.printStackTrace();
+        }
+    }
 
+    private Connection obtenerConexion() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/supermercado";
+        String user = "admin";
+        String password = "dm2";
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    private void mostrarError() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setTitle("ERROR");
+        alert.setContentText("Ha ocurrido un error");
+        alert.showAndWait();
+    }
+}
